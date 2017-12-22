0df5dfaa4de59d4db4cf163b34f98d2d11e81761
GG <33389734+goddzi@users.noreply.github.com>
2017-12-22 15:35:55 +0900

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -10,12 +10,7 @@ hpcN[,1]<-as.POSIXct(paste(hpcN[,1],hpcN[,2]))
 hpcN[,2]<-NULL
 
 ## plot data
-with(hpcN, {plot(Sub_metering_1~Date,type="l",xlab = "",ylab = "Energy sub metering")
-  lines(Sub_metering_2~Date,col="red")
-  lines(Sub_metering_3~Date,col="blue")
-  legend("topright", legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"),col = c("black","red","blue")
-         , lty=1, lwd=2, bty="n")
-})
+plot(hpcN$Date,hpcN$Global_active_power,type="l",xlab = "",ylab = "Global Active Power (kilowatts)")
 
 ## write plot data to file
 dev.copy(png,"plot3.png", height = 480 , width = 480)
